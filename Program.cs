f6013aa8e6f5b8b7fb70cabcbb92588db9adb97d
Felipe Fonseca <felipe-fon@hotmail.com>
2021-01-24 14:54:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CWI_SellAnalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
